41fb4b69d1ba725de1a9b22ebb61d86417e2adec
Mert Koksal <mert.koksal@hamurlabs.com>
2020-09-09 16:14:20 +0300

    final

---
+++ ComplexUI3/Coordinator.swift
@@ -0,0 +1,21 @@
+//
+//  Coordinator.swift
+//  ComplexUI3
+//
+//  Created by Mert Köksal on 9.09.2020.
+//  Copyright © 2020 Mert Köksal. All rights reserved.
+//
+
+import SwiftUI
+
+struct Coordinator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Coordinator_Previews: PreviewProvider {
+    static var previews: some View {
+        Coordinator()
+    }
+}
---
+++ ComplexUI3/Service.swift
@@ -0,0 +1,9 @@
+//
+//  Service.swift
+//  ComplexUI3
+//
+//  Created by Mert Köksal on 9.09.2020.
+//  Copyright © 2020 Mert Köksal. All rights reserved.
+//
+
+import Foundation

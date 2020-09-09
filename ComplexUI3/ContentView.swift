f94188b8e457318b60f16c045d492af4394cc371
Mert Koksal <mert.koksal@hamurlabs.com>
2020-09-09 10:01:04 +0300

    Initial Commit

---
+++ ComplexUI3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComplexUI3
+//
+//  Created by Mert Köksal on 9.09.2020.
+//  Copyright © 2020 Mert Köksal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
